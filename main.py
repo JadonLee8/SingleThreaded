e7fc93e78025ceb753293d6b0430c5bd6eb5c489
Jadon Lee <91638482+JadonLee8@users.noreply.github.com>
2023-10-05 19:16:53 +0000

    changes

--- main.py
+++ main.py
@@ -1,12 +1,12 @@
 import heapq
+import sys
 import cv2 as cv
 import numpy as np
 
-img = cv.imread("mona_lisa.jpg", cv.IMREAD_GRAYSCALE)
+img = cv.imread("eye.jpg", cv.IMREAD_GRAYSCALE)
 
 # the smaller side of the image fed to the program will be the radius of the circle
 radius = min(img.shape)
-print(radius)
 
 # crop the image to a square with side length equal to the radius.
 cropped_img = img[(int)(img.shape[0]/2 - radius/2):(int)(img.shape[0]/2 + radius/2), (int)(img.shape[1]/2 - radius/2):(int)(img.shape[1]/2 + radius/2)]
@@ -25,7 +25,6 @@ for i in range(num_pins):
 
 
 # calculate priority queues for each pin
-
 # NOTE: will use a linked list in the future when I consider the line against previous lines, not just the previous image
 priority_queues = []
 current_a_pin = 0
@@ -33,11 +32,14 @@ for pin_from in pins:
     priority_queue = []
     current_b_pin = 0
     for pin_to in pins:
+        if (current_a_pin == current_b_pin):
+            current_b_pin += 1
+            continue
         # black on white lines
         # line_image = np.full((radius, radius), 255, dtype=np.uint8)
         copy = np.copy(cropped_img)
         line_image = cv.line(copy, (pins[current_a_pin]), (pins[current_b_pin]), 0, 1)
-        COMPUTED_COST = np.average(np.abs(np.subtract(line_image, cropped_img)))
+        COMPUTED_COST = np.sum(cv.absdiff(line_image, cropped_img))/np.linalg.norm(np.array(pins[current_a_pin]) - np.array(pins[current_b_pin]))
         heapq.heappush(priority_queue, (COMPUTED_COST, pin_to)) 
         current_b_pin += 1
         del line_image
@@ -45,10 +47,12 @@ for pin_from in pins:
     priority_queues.append(priority_queue)
     del priority_queue
     current_a_pin += 1
-    print(current_a_pin/num_pins*100, "% \done of priority queues")
+    sys.stdout.write('\r')
+    sys.stdout.write(f'Computing line costs... {(int)(current_a_pin/len(pins)*100)}%')
+    sys.stdout.flush()
 
 final_image = np.full((radius, radius), 255, dtype=np.uint8)
-num_lines = 4000
+num_lines = 1300
 start_pin = 0
 for i in range(num_lines):
     this_pins_pq = priority_queues[start_pin]
@@ -59,10 +63,13 @@ for i in range(num_lines):
     #     continue
     final_image = cv.line(final_image, pins[start_pin], destination[1], 0, 1)
     start_pin = pins.index(destination[1])
-    print(i/num_lines*100, "% \done of final image")
 
-cv.imshow("pinned image", pinned_image)
-cv.imshow("final image", final_image)
+    sys.stdout.write('\r')
+    sys.stdout.write(f'Printing image... {(int)(i/num_lines*100)}%')
+    sys.stdout.flush()
+
+cv.imwrite("image_pins.png", pinned_image)
+cv.imwrite("final.png", final_image)
 
 # wait until window is closed to stop GUI
 cv.waitKey(0)
